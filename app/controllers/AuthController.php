40a5fe2441be7631fe2d098b7a20bbb3ba940487
trananhtu04072001 <04072001trananhtu@gmail.com>
2025-07-16 14:18:15 +0700

    first commit

---
+++ app/controllers/AuthController.php
@@ -0,0 +1,14 @@
+<?php
+
+class AuthController extends ControllerBase
+{
+    public function loginAction()
+    {
+        echo 'Trang dang nhap';
+    }
+
+    public function registerAction() {
+        echo 'man dang ky';
+    }
+}
+
---
+++ cache/c__xampp_htdocs_phalcon_dev_app_views_auth_register.volt.php
@@ -0,0 +1,26 @@
+<div class="container-form-register mx-auto h-100">
+    <h2 class="text-center">Đăng ký tài khoản</h2>
+
+    <?= $this->flashSession->output() ?>
+    <div>
+        <form method="post" action="<?= $this->url->get('auth/register') ?>">
+        <div class="mb-3">
+            <label class="form-label">Tên đăng nhập</label>
+            <input class="form-control" type="text" name="username" required>
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Email</label>
+            <input class="form-control" type="email" name="email" required>
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Mật khẩu</label>
+            <input class="form-control" type="password" name="password" required>
+        </div>
+            <div class="mb-3">
+            <label class="form-label">Nhập lại mật khẩu</label>
+            <input class="form-control" type="password" name="confirm_password" required>
+        </div>
+        <button class="btn btn-primary m-auto" type="submit">Đăng ký</button>
+    </div>
+    </form>
+</div>
